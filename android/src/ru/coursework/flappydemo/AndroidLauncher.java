72cdc28ae123a7de541c656f17cde529060e9b2b
EuleSkelett <64166286+EuleSkelett@users.noreply.github.com>
2020-05-13 19:47:27 +0300

    1

---
+++ android/src/ru/coursework/flappydemo/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package ru.coursework.flappydemo;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.coursework.flappydemo.FlappyDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useCompass = false;
+		config.useAccelerometer = false;
+		initialize(new FlappyDemo(), config);
+	}
+}
